4b8fb67c6e442550c61385f346215545f346c99f
ninox92 <thomasreynders@hotmail.nl>
2014-07-23 11:25:20 +0200

    v1.1
    New css styles
    Animations fixed
    Smoother transitions over different animations

--- ripple.js
+++ ripple.js
@@ -3,9 +3,10 @@
  * By Thomas Reynders http://thomasreynders.com
  * MIT Licensed.
  * 
- * Version 1.0
+ * Version 1.1
  * based on : http://thecodeplayer.com/walkthrough/ripple-click-effect-google-material-design
- * https://github.com/ninox92/Google-material-design-ripple-effect/
+ * Github: https://github.com/ninox92/Google-material-design-ripple-effect/
+ * preview: http://thomasreynders.com/google-material-design-plugin/
  */
 (function ( $ ) {
 	
@@ -22,9 +23,9 @@
 	$.RippleEffect.prototype = {
 		defaults:{
 			//DO NOT CHANGE ORDER
+			siblingElement:"<span class='ink'></span>",
 			classes: ['animate', 'focus'],//animation classes
-			sibling: '.ink',
-			focusTimeOut: 500
+			sibling: '.ink'
 		},
 		data:{},//data from options
 		init: function(o) {
@@ -47,74 +48,70 @@
 			//bind mouse down event to elements object from data
 			if(this.data.elements){
 				$(this.data.elements).mousedown(function(e){
-					
 					_.animate($(this), e, _.data.classes[0]);// make it animate
 				});
 			} else {
 				throw new TypeError("Function.prototype.bind - Please set up the options for RippleEffect correctly! No elements are set, new $.RippleEffect({'elements':'.btn, ul li a'})"); 
 			}
+			//add focus if used
 			if(this.data.focus){
-				
-				$(this.data.focus).focus(function(e){
-					//add focus state to a element
-					var _e = e,
-						$t = $(this);
-						
-					setTimeout(function(){
-						_.animate($t, _e, _.data.classes[1]);
-					},_.data.focusTimeOut);
-					
-				});
-				//remove the class on focus out
-				$(this.data.focus).focusout(function(){
-					$(this).find(_.data.sibling).removeClass(_.data.classes[1]);
-				});
-			}
+				this.focus();
+			}	
+		},
+		focus:function(){
+			var _ = this;
 			
+			$(this.data.focus).focus(function(e){
+				//add focus state to a element
+				var _e = e,
+					$t = $(this);
+				
+				//on animation-end animate the sibling focus animation.
+				$(this).find(_.data.sibling).one('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function() {
+				    _.animate($t, _e, _.data.classes[1]);
+				});
+				
+			}).focusout(function(){
+			//remove css classes on focus out
+				$(this).find(_.data.sibling).removeCssClasses(_.data.classes);
+			});
 		},
 		animate: function(el, e, cssclass){
-			tag = el.get(0).tagName;
-			switch(tag){//check for what sort of element is clicked
-				case "A":
-				parent = el.parent();
-					break;
-				case "BUTTON":
-				parent = el;
-					break;
-				default:
-				parent = el;
-					break;
-			}
-			//create .ink element if it doesn't exist
-			if(parent.find(this.data.sibling).length == 0)
-				parent.prepend("<span class='ink'></span>");
-				
-			ink = parent.find(this.data.sibling);
-			//incase of quick double clicks stop the previous animation
-			this.removeCssClasses();
-			//ink.removeClass(cssclass);
+			var _ = this;
 			
-			//set size of .ink
+			//create sibling element if it doesn't exist
+			if(el.find(this.data.sibling).length == 0){
+				el.prepend( this.data.siblingElement );
+			}
+			
+			//find sibling of parent
+			ink = el.find(this.data.sibling);
+			//incase of quick double clicks stop the previous animation
+			el.removeCssClasses(this.data.classes);
+			ink.removeCssClasses(this.data.classes);
+			
+			//set size of .ink 
 			ink.resetDimensions();
 			
 			//get click coordinates
 			//set to the middle of the element
 			//set the position and add class .animate
-			if(cssclass == this.data.classes[0]){
-				x = e.pageX - parent.offset().left - ink.width()/2;
-				y = e.pageY - parent.offset().top - ink.height()/2;
+			if(cssclass == this.data.classes[0]){//if animate
+				x = e.pageX - el.offset().left - ink.width()/2;
+				y = e.pageY - el.offset().top - ink.height()/2;
+				el.addClass(cssclass);//animate button background color
 				ink.css({top: y+'px', left: x+'px'}).addClass(cssclass);
-			} else {
+			} else {//if focus
 				ink.center().addClass(cssclass);
 			}
-		},
-		removeCssClasses: function(){
-			var c = this.data.classes;
-			for(var i=0; i<c.length; i++){
-				if( ink.hasClass(c[i]) ) ink.removeClass(c[i]);
-			}
-		},
-		
+		}
+	};
+	
+	jQuery.fn.removeCssClasses = function(classes, isSVG){
+		//remove all css classes that are equal to the data.classes object
+		for(var i=0; i<classes.length; i++){
+			if($(this).hasClass(classes[i])) $(this).removeClass(classes[i]);
+		}
 	};
 	jQuery.fn.center = function () {
 		//Center the element based on parent element
@@ -122,7 +119,6 @@
 		this.css("left",(this.parent().outerWidth() - this.outerWidth()) + 'px');
 		return this;
 	};
-
 	jQuery.fn.resetDimensions = function(){
 		//use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
 		d = Math.max($(this).parent().outerWidth(), $(this).parent().outerHeight());
